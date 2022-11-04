bdf1231a4d995ba7d9000fe94df91337f2526214
Oscar Rodriguez <oxkar991@hotmail.com>
2022-11-03 19:53:59 -0500

    Primer commit

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <Navbar />
+
+  <div class="d-flex">
+    <div class="col-4">
+      <EntryList />
+    </div>
+    <div class="col">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import { defineAsyncComponent } from "vue"; /* defineAsyncComponent me permite tener menos importanciones en los script y facilita cambiar
+nombres a los componentes */
+
+export default {
+  components: {
+    Navbar: defineAsyncComponent(() => import("../components/Navbar.vue")),
+    EntryList: defineAsyncComponent(() =>
+      import("../components/EntryList.vue")
+    ),
+  },
+};
+</script>
